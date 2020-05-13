96e3cca3a6c4f76ab4cfcdb64c90be721e48d672
akorol98 <akorol98.17@gmail.com>
2020-05-13 21:27:00 +0200

    load several channels

--- dataUtils/CustomDataset.py
+++ dataUtils/CustomDataset.py
@@ -38,36 +38,67 @@ class IonSwitchingDataset(Dataset):
 
         n_before = int(self.size_w*self.slice_ratio)
         n_after = self.size_w-n_before
+        columns = [0,1,2,3,4]
+        input_list = np.empty((0, self.size_w+1))
         
-
         if idx-n_before < 0:
             n_to_concat = n_before - idx
-            signal = self.data[:idx+1+n_after, 1]
-            signal = np.concatenate(
-                (self.data[:n_to_concat, 1], signal),
-                axis=0
-            )
-        elif idx+n_after > len(self.data)-1:
-            n_to_concat = n_after - (len(self.data)-idx)
-            signal = self.data[idx-n_before:, 1]
-            if n_to_concat > 0:
-                signal = np.concatenate(
-                    (signal, self.data[-n_to_concat-1:, 1]),
+            for i in columns:
+                input = self.data[:idx+1+n_after, i]
+                input = np.concatenate(
+                    (self.data[:n_to_concat, i], input),
                     axis=0
                 )
+                input = input.reshape(1, len(input))
+                input_list = np.append(input_list, input, axis=0)
+        elif idx+n_after > len(self.data)-1:
+            n_to_concat = n_after - (len(self.data)-idx)
+            for i in columns:
+                input = self.data[idx-n_before:, i]
+                input = np.concatenate(
+                    (input, self.data[-n_to_concat-1:, i]),
+                    axis=0
+                )
+                input = input.reshape(1, len(input))
+                input_list = np.append(input_list, input, axis=0)
         else:
-            signal = self.data[np.r_[idx-n_before:idx, idx, idx+1:idx+1+n_after], 1]
+            for i in columns:
+                input = self.data[np.r_[idx-n_before:idx, idx, idx+1:idx+1+n_after], i]
+                input = input.reshape(1, len(input))
+                input_list = np.append(input_list, input, axis=0)
+            
+#         if idx-n_before < 0:
+#             n_to_concat = n_before - idx
+#             signal = self.data[np.r_[0:idx, idx+1:idx+1+n_after], 2]
+#             signal = np.concatenate(
+#                 (self.data[:n_to_concat, 2], signal),
+#                 axis=0
+#             )
+#         elif idx+n_after > len(self.data)-1:
+#             n_to_concat = n_after - (len(self.data)-idx)
+#             signal = self.data[np.r_[idx-n_before:idx, idx+1:len(self.data)], 2] 
+#             signal = np.concatenate(
+#                 (signal, self.data[-n_to_concat-1:, 2]),
+#                 axis=0
+#             )
+#         else:
+#             signal = self.data[np.r_[idx-n_before:idx, idx+1:idx+1+n_after], 2]
             
 #         signal = 2*(signal-signal.min())/(signal.max()-signal.min())-1
         
+#         p0 = p0.reshape(1, len(p0))
+#         signal = signal.reshape(1, len(signal))
+#         input = np.concatenate((signal,p0), axis=0)
+    
         if self.train:
-            n_open_channels = int(self.data[idx, 2])
+            n_open_channels = int(self.data[idx, -1])
             open_channels = torch.tensor(np.zeros(11))
             open_channels[n_open_channels] = 1
-
-            sample = {'signal': signal,
-                      'open_channels': open_channels}
-        else: sample = {'signal': signal}
+            
+            sample = {'input': input_list,
+                      'open_channels': open_channels
+                     }
+        else: sample = {'input': input_list}
 
         if self.transform:
             sample = self.transform(sample)
@@ -80,10 +111,13 @@ class ToTensor(object):
 
     def __call__(self, sample):
         if len(sample) > 1:
-            signal, open_channels = sample['signal'], sample['open_channels']
-            signal = torch.from_numpy(signal)
+            input = sample['input']
+            open_chennels = sample['open_channels']
+            input = torch.from_numpy(input)
             open_channels = torch.from_numpy(open_channels)
-            return {'signal': signal, 'open_channels': open_channels}
+            return {'input': input,
+                    'open_channels': open_channels
+                   }
         else:
             signal = sample['signal']
             signal = torch.from_numpy(signal)
